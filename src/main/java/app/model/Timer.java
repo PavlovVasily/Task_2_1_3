a6414be59460bc71cc287741970e0321123a3429
PavlovVasiliy <vasylyjpavlov@mail.ru>
2022-02-08 21:53:09 +0300

    Task_2_1_3 Done

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,11 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+//@Scope(value = "prototype")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
